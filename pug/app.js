8e1a46fb1e5c1e612031fc72d283e6e6627c22e4
Soledad Alaniz <sole_lr@hotmail.com>
2022-05-26 23:18:17 -0300

    configurando servidor

---
+++ pug/app.js
@@ -0,0 +1,28 @@
+const express= require("express");
+const path = require("path");
+const PORT=8080;
+
+const app= express();
+
+
+
+const pathStatic = path.resolve(__dirname, "./public");
+
+
+
+// ************ Para usar Post ************
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.json());
+
+
+app.use(express.static(pathStatic));
+
+// Template Engine
+app.set('view engine', 'pug');
+
+app
+.listen(PORT,()=>{
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+})
+.on('error',error=>console.log(`error del servidor:${error}`))
