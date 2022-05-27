ac44513deb5acd72a97572ab1612f34986c1d246
Soledad Alaniz <sole_lr@hotmail.com>
2022-05-26 22:39:12 -0300

    dinamismo

--- handlebars/routes/productsRoutes.js
+++ handlebars/routes/productsRoutes.js
@@ -20,14 +20,18 @@ const uploadFile = multer({ storage });
 
 let productos = []
 
-/* router.get('', (req, res) => {
-  return res.json(productos)
-}) */
+
 router.get('', (req, res) => {
     return res.render("layout/formProduct")
 })
 router.get('/products', (req, res) => {
-    return res.render("layout/listProduct", { productos })
+    if (productos.length>0)
+        return res.render("layout/listProduct", { productos })
+    else
+    return res.render("layout/listProduct", { msg: "No hay productos cargados" })
+
+
+
 })
 router.post('', uploadFile.single('image'), (req, res) => {
 
@@ -41,64 +45,5 @@ router.post('', uploadFile.single('image'), (req, res) => {
 
     return res.render("layout/listProduct", { productos })
 })
-/* router.get("/:id", (req, res) => {
-  id = req.params.id;
-  let productToId = productos.find(product => product.id == id);
-  if (productToId) {
-    return res.json(productToId)
-  }
-  else {
-    return res.json({ 'error': 'Producto no encontrado' })
 
-  }
-
-})
-
-router.post('', uploadFile.single('image'), (req, res) => {
-
-    let newProduct = {
-      id: productos.length + 1,
-      ...req.body,
-      image: req.file.filename
-    };
-    productos.push(newProduct);
-  
-
- 
-
-  return res.status(201).json(newProduct)
-})
-
-router.put('/:id', (req, res) => {
-  id = req.params.id;
-  let productToEdit = productos.find(product => product.id == id)
-  if (productToEdit) {
-    productToEdit = {
-      id: productToEdit.id,
-      ...req.body,
-    }
-
-    let newProducts = productos.map(product => {
-      if (product.id == productToEdit.id) {
-        return product = { ...productToEdit };
-      }
-      return product;
-    })
-
-
-    return res.json(newProducts)
-  }
-  else {
-    return res.json({ 'error': 'Producto no encontrado' })
-  }
-
-
-})
-
-router.delete('/:id', (req, res) => {
-  let id = req.params.id;
-  let finalProducts = productos.filter(product => product.id != id);
-  return res.json(finalProducts)
-})
- */
 module.exports = router;
